bdc2ec592f8495aff6648fdfd4608545ba8219a1
lasseslips <lasseslips@gmail.com>
2023-11-15 15:59:28 +0100

    defines

--- src/main/scala/riscv/Alu.scala
+++ src/main/scala/riscv/Alu.scala
@@ -2,6 +2,7 @@ package riscv
 
 import chisel3.util._
 import chisel3._
+import riscv.AluType._
 
 class Alu extends Module {
   val io = IO(new Bundle() {
@@ -10,16 +11,6 @@ class Alu extends Module {
     val res = Output(UInt(32.W))
     val control = Input(UInt(5.W))
   })
-  val Add = 0.U
-  val Sub = 1.U
-  val Sll = 2.U
-  val Xor = 3.U
-  val Srl = 4.U
-  val Sra = 5.U
-  val Or = 6.U
-  val And = 7.U
-  val Slt = 8.U
-  val Sltu = 9.U
 
 
   // default value
@@ -27,34 +18,34 @@ class Alu extends Module {
 
   //ADD
   switch (io.control) {
-    is(Add) {
+    is(ADD.id.U) {
       io.res := io.op1 + io.op2
    }
-    is(Sub) {
+    is(SUB.id.U) {
       io.res := io.op1 - io.op2
     }
-    is(Sll) {
+    is(SLL.id.U) {
       io.res := io.op1 << io.op2(4,0)
     }
-    is(Xor) {
+    is(XOR.id.U) {
       io.res := io.op1 ^ io.op2
     }
-    is(Srl) {
+    is(SRL.id.U) {
       io.res := io.op1 >> io.op2(4,0)
     }
-    is(Sra) {
+    is(SRA.id.U) {
       io.res := (io.op1.asSInt >> io.op2(4,0)).asUInt
     }
-    is(Or) {
+    is(OR.id.U) {
       io.res := io.op1 | io.op2
     }
-    is(And) {
+    is(AND.id.U) {
       io.res := io.op1 & io.op2
     }
-    is(Slt) {
+    is(SLT.id.U) {
       io.res := (io.op1.asSInt < io.op2.asSInt).asUInt
     }
-    is(Sltu) {
+    is(SLTU.id.U) {
       io.res := (io.op1 < io.op2).asUInt
     }
   }
---
+++ src/main/scala/riscv/defines.scala
@@ -0,0 +1,57 @@
+package riscv
+
+object Opcode {
+  val AluImm = 0x13
+  val Alu = 0x33
+  val Branch = 0x63
+  val Load = 0x03
+  val Store = 0x23
+  val Lui = 0x37
+  val AuiPc = 0x17
+  val Jal = 0x6f
+  val JalR = 0x67
+  val Fence = 0x0f
+  val ECall = 0x73
+}
+object InstrType extends Enumeration {
+  type InstrType = Value
+  val R, I, S, SB, U, UJ = Value
+}
+
+object AluType extends Enumeration {
+  type AluType = Value
+  val ADD, SUB, SLL, XOR, SRL, SRA, OR, AND, SLT, SLTU = Value
+}
+
+object AluFunct7 {
+  val DEFAULT = 0x00
+  val SRA_SUB = 0x20
+}
+
+object AluFunct3 {
+  val F3_ADD_SUB = 0x00 // no SUB in I-type
+  val F3_SLL = 0x01
+  val F3_SLT = 0x02
+  val F3_SLTU = 0x03
+  val F3_XOR = 0x04
+  val F3_SRL_SRA = 0x05
+  val F3_OR = 0x06
+  val F3_AND = 0x07
+}
+
+object BranchFunct {
+  val BEQ = 0x00
+  val BNE = 0x01
+  val BLT = 0x04
+  val BGE = 0x05
+  val BLTU = 0x06
+  val BGEU = 0x07
+}
+
+object LoadStoreFunct {
+  val LSB = 0x00
+  val LSH = 0x01
+  val LSW = 0x02
+  val LBU = 0x04
+  val LHU = 0x05
+}
