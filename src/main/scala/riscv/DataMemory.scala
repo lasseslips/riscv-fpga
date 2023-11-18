ccc504df4c4f605eea00d75e9c81473a77a9de0a
lasseslips <lasseslips@gmail.com>
2023-11-18 13:59:25 +0100

    rewriting datamemory again using instruction as input

--- src/main/scala/riscv/DataMemory.scala
+++ src/main/scala/riscv/DataMemory.scala
@@ -6,27 +6,93 @@ class DataMemory extends Module {
   val io = IO(new Bundle() {
     val addr = Input(UInt(20.W))
     val write = Input(Bool())
-    val dataIn = Input(Vec(4,UInt(8.W)))
-    val dataOut = Output(Vec(4,UInt(8.W)))
-    val mask = Input(Vec(4,Bool()))
+    val dataIn = Input(UInt(32.W))
+    val dataOut = Output(UInt(32.W))
+    val insType = Input(UInt(3.W))
   })
 
+  io.dataOut := DontCare
+  val dataInVec = Wire(Vec(4, UInt(8.W)))
+  dataInVec(0) := io.dataIn(7,0)
+  dataInVec(1) := io.dataIn(15,8)
+  dataInVec(2) := io.dataIn(23,16)
+  dataInVec(3) := io.dataIn(31,24)
+  val dataOutVec = Wire(Vec(4,UInt(8.W)))
+
+  val mask = Wire(Vec(4, Bool()))
+  mask := DontCare
+  val signBit = Wire(Bool())
+  signBit := DontCare
 
 
+  switch(io.insType) {
+    is(LoadStoreFunct.LB_SB.U) {
+      mask(0) := true.B
+      mask(1) := false.B
+      mask(2) := false.B
+      mask(3) := false.B
+    }
+    is(LoadStoreFunct.LH_SH.U) {
+      mask(0) := true.B
+      mask(1) := true.B
+      mask(2) := false.B
+      mask(3) := false.B
+    }
+    is(LoadStoreFunct.LW_SW.U) {
+      mask(0) := true.B
+      mask(1) := true.B
+      mask(2) := true.B
+      mask(3) := true.B
+    }
+    is(LoadStoreFunct.LBU.U) {
+      mask(0) := true.B
+      mask(1) := false.B
+      mask(2) := false.B
+      mask(3) := false.B
+    }
+    is(LoadStoreFunct.LHU.U) {
+      mask(0) := true.B
+      mask(1) := true.B
+      mask(2) := false.B
+      mask(3) := false.B
+    }
+  }
   // 1,048,576 x 32 bit = 32Mb
   val mem = SyncReadMem(Math.pow(2, 20).toInt, Vec(4, UInt(8.W)))
 
   io.dataOut := DontCare
   when(io.write) {
-    mem.write(io.addr,io.dataIn,io.mask)
+    mem.write(io.addr,dataInVec,mask)
   }
   val data = mem.read(io.addr)
 
   for (i <- 0 until 4) {
-    when(io.mask(i)) {
-      io.dataOut(i) := data(i)
+    when(mask(i)) {
+      dataOutVec(i) := data(i)
     } .otherwise {
-      io.dataOut(i) := 0.U
+      dataOutVec(i) := 0.U
+    }
+  }
+
+  switch(io.insType) {
+    is(LoadStoreFunct.LB_SB.U) {
+      signBit := dataOutVec(0)(7)
+      io.dataOut := Cat(Fill(24,signBit), dataOutVec(0))
+    }
+    is(LoadStoreFunct.LH_SH.U) {
+      signBit := dataOutVec(1)(7)
+      io.dataOut := Cat(Fill(16,signBit), dataOutVec(1),dataOutVec(0))
+    }
+    is(LoadStoreFunct.LW_SW.U) {
+      io.dataOut := Cat(dataOutVec)
+    }
+    is(LoadStoreFunct.LBU.U) {
+      signBit := false.B
+      io.dataOut := Cat(Fill(24,signBit),dataOutVec(0))
+    }
+    is(LoadStoreFunct.LHU.U) {
+      signBit := false.B
+      io.dataOut := Cat(Fill(16,signBit), dataOutVec(1),dataOutVec(0))
     }
   }
 
--- src/test/scala/Memory/DataMemoryTest.scala
+++ src/test/scala/Memory/DataMemoryTest.scala
@@ -3,37 +3,28 @@ package Memory
 import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
-import riscv.DataMemory
+import riscv.{DataMemory, LoadStoreFunct}
 
 class DataMemoryTest extends AnyFlatSpec with ChiselScalatestTester{
   "DataMemory" should "Pass" in {
     test(new DataMemory) { dut =>
       dut.io.addr.poke("hff".U)
       dut.io.write.poke(true.B)
-
-      dut.io.dataIn(0).poke(1.U)
-      dut.io.dataIn(1).poke(2.U)
-      dut.io.dataIn(2).poke(3.U)
-      dut.io.dataIn(3).poke(4.U)
-
-      dut.io.mask(0).poke(true.B)
-      dut.io.mask(1).poke(true.B)
-      dut.io.mask(2).poke(true.B)
-      dut.io.mask(3).poke(true.B)
+      dut.io.insType.poke(LoadStoreFunct.LW_SW)
+      dut.io.dataIn.poke("hffffffff".U)
 
       dut.clock.step(1)
       dut.io.addr.poke("hff".U)
       dut.io.write.poke(false.B)
+      dut.io.insType.poke(LoadStoreFunct.LB_SB)
+      dut.io.dataOut.expect("hFFFFFFFF".U)
+      dut.clock.step(1)
+      dut.io.addr.poke("hff".U)
+      dut.io.write.poke(false.B)
+      dut.io.insType.poke(LoadStoreFunct.LBU)
+      dut.io.dataOut.expect("hFF".U)
+      dut.clock.step(2)
 
-      dut.io.mask(0).poke(false.B)
-      dut.io.mask(1).poke(false.B)
-      dut.io.mask(2).poke(true.B)
-      dut.io.mask(3).poke(false.B)
-
-      dut.io.dataOut(0).expect(0.U)
-      dut.io.dataOut(1).expect(0.U)
-      dut.io.dataOut(2).expect(3.U)
-      dut.io.dataOut(3).expect(0.U)
     }
   }
 }
