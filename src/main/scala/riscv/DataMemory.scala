2e642485c4662e5e371e505a39f84ed2127037a9
lasseslips <lasseslips@gmail.com>
2023-11-18 11:41:21 +0100

    redone the datamemory using masks

--- src/main/scala/riscv/DataMemory.scala
+++ src/main/scala/riscv/DataMemory.scala
@@ -5,23 +5,29 @@ import chisel3.util._
 class DataMemory extends Module {
   val io = IO(new Bundle() {
     val addr = Input(UInt(20.W))
-    val enable = Input(Bool())
     val write = Input(Bool())
-    val dataIn = Input(UInt(8.W))
-    val dataOut = Output(UInt(8.W))
+    val dataIn = Input(Vec(4,UInt(8.W)))
+    val dataOut = Output(Vec(4,UInt(8.W)))
+    val mask = Input(Vec(4,Bool()))
   })
 
 
-  // 1,048,576 x 8 bit = 8Mb
-  val mem = SyncReadMem(Math.pow(2, 20).toInt, UInt(8.W))
+
+  // 1,048,576 x 32 bit = 32Mb
+  val mem = SyncReadMem(Math.pow(2, 20).toInt, Vec(4, UInt(8.W)))
+
   io.dataOut := DontCare
-  when(io.enable) {
-    val rdwrPort = mem(io.addr)
-    when (io.write) {
-      rdwrPort := io.dataIn
+  when(io.write) {
+    mem.write(io.addr,io.dataIn,io.mask)
+  }
+  val data = mem.read(io.addr)
+
+  for (i <- 0 until 4) {
+    when(io.mask(i)) {
+      io.dataOut(i) := data(i)
     } .otherwise {
-      io.dataOut := rdwrPort
+      io.dataOut(i) := 0.U
     }
   }
-  io.dataOut := mem.read(io.addr, true.B)
+
 }
--- src/test/scala/Memory/DataMemoryTest.scala
+++ src/test/scala/Memory/DataMemoryTest.scala
@@ -8,23 +8,32 @@ import riscv.DataMemory
 class DataMemoryTest extends AnyFlatSpec with ChiselScalatestTester{
   "DataMemory" should "Pass" in {
     test(new DataMemory) { dut =>
+      dut.io.addr.poke("hff".U)
       dut.io.write.poke(true.B)
-      dut.io.addr.poke("hFF".U)
-      dut.io.dataIn.poke(123.U)
+
+      dut.io.dataIn(0).poke(1.U)
+      dut.io.dataIn(1).poke(2.U)
+      dut.io.dataIn(2).poke(3.U)
+      dut.io.dataIn(3).poke(4.U)
+
+      dut.io.mask(0).poke(true.B)
+      dut.io.mask(1).poke(true.B)
+      dut.io.mask(2).poke(true.B)
+      dut.io.mask(3).poke(true.B)
+
       dut.clock.step(1)
-      dut.io.enable.poke(true.B)
-      dut.clock.step(1)
-      dut.io.enable.poke(false.B)
-      dut.clock.step(1)
+      dut.io.addr.poke("hff".U)
       dut.io.write.poke(false.B)
-      dut.clock.step(1)
-      dut.io.enable.poke(true.B)
-      dut.io.dataOut.expect(123.U)
 
+      dut.io.mask(0).poke(false.B)
+      dut.io.mask(1).poke(false.B)
+      dut.io.mask(2).poke(true.B)
+      dut.io.mask(3).poke(false.B)
 
-
-
-
+      dut.io.dataOut(0).expect(0.U)
+      dut.io.dataOut(1).expect(0.U)
+      dut.io.dataOut(2).expect(3.U)
+      dut.io.dataOut(3).expect(0.U)
     }
   }
 }
