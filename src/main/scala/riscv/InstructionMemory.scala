ffdcbea4d058d5abc0996ff113b49fec54d2253a
lasseslips <lasseslips@gmail.com>
2023-11-20 18:01:27 +0100

    simple instructionmemory for testing

--- src/main/scala/riscv/InstructionMemory.scala
+++ src/main/scala/riscv/InstructionMemory.scala
@@ -1,9 +1,23 @@
 package riscv
 
 import chisel3._
+import chisel3.util._
 class InstructionMemory extends Module {
   val io = IO(new Bundle() {
-
+    val addr = Input(UInt(16.W))
+    val dataOut = Output(UInt(32.W))
+    val dataIn = Input(UInt(32.W))
+    val write = Input(Bool())
   })
 
+
+  io.dataOut := DontCare
+  // 262kB
+  val mem = SyncReadMem(Math.pow(2,16).toInt,UInt(32.W))
+
+  when(io.write) {
+    mem.write(io.addr,io.dataIn)
+  } .otherwise {
+    io.dataOut := mem.read(io.addr, true.B)
+  }
 }
---
+++ src/test/scala/Memory/InstructionMemoryTest.scala
@@ -0,0 +1,20 @@
+package Memory
+
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+import riscv.InstructionMemory
+
+class InstructionMemoryTest extends AnyFlatSpec with ChiselScalatestTester{
+  "InstructionMemory" should "Pass" in {
+    test(new InstructionMemory) { dut =>
+      dut.io.write.poke(true.B)
+      dut.io.dataIn.poke("hffff".U)
+      dut.io.addr.poke(0.U)
+      dut.clock.step()
+      dut.io.write.poke(false.B)
+      dut.clock.step(10)
+      dut.io.dataOut.expect("hffff".U)
+    }
+  }
+}
